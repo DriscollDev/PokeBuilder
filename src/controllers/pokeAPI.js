fb09835eaa64642ffecd78d6bd32e63f28dd43d1
DriscollDev <cdriscolldev@gmail.com>
2025-01-30 09:42:57 -0500

    options

--- src/controllers/pokeAPI.js
+++ src/controllers/pokeAPI.js
@@ -1,5 +1,12 @@
 import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
+const options = {
+    protocol: 'https',
+    //hostName: 'localhost:443',
+    versionPath: '/api/v2/',
+    cacheLimit: 100 * 1000, // 100s
+    timeout: 5 * 1000 // 5s
+  }
+const P = new Pokedex(options);
 
 const pokeAPI = {
     
